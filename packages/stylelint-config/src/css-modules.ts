27a18d5df8bb9f36a11a9cf8b0fee7afe6435a28
Carl Hamilton <carlanthonyhamilton@gmail.com>
2021-03-16 19:59:08 +0000

    fix(stylelint): fix config

--- packages/stylelint-config/src/css-modules.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import { Configuration } from "stylelint";
-
-const config: Partial<Configuration> = {
-  extends: ["@cahamilton/stylelint-config", "stylelint-config-css-modules"],
-};
-
-module.exports = config;
--- packages/stylelint-config/src/index.ts
+++ packages/stylelint-config/src/index.ts
@@ -5,6 +5,8 @@ import { Configuration } from "stylelint";
 const config: Partial<Configuration> = {
   extends: [
     "stylelint-config-standard",
+    "stylelint-config-css-modules",
+    "stylelint-config-styled-components",
     "stylelint-config-property-sort-order-smacss",
   ],
 };
