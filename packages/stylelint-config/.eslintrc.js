066547b6852fb1b76db74d5d9e2637a49788a888
Carl Hamilton <carlanthonyhamilton@gmail.com>
2021-11-06 18:38:57 +0000

    chore: lerna link convert

--- packages/stylelint-config/.eslintrc.js
+++ packages/stylelint-config/.eslintrc.js
@@ -1,9 +1,22 @@
 /** @format */
 
+const path = require("path");
+
 module.exports = {
   extends: "@cahamilton/eslint-config",
   parserOptions: {
     project: "tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        packageDir: [
+          path.resolve(__dirname),
+          path.resolve(__dirname, "../../"),
+        ],
+      },
+    ],
   },
 };
