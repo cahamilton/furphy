02795f96e100d02674d2053d725e537b14a6d232
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2026-02-28 22:28:34 +0000

    feat!: update stylelint config (#19)

--- packages/stylelint-config/src/index.ts
+++ packages/stylelint-config/src/index.ts
@@ -5,22 +5,10 @@ import type { Config } from "stylelint";
 const config: Config = {
   extends: [
     "stylelint-config-standard",
+    "stylelint-config-standard-scss",
     "stylelint-config-css-modules",
-    "stylelint-config-styled-components",
     "stylelint-config-property-sort-order-smacss",
-  ],
-  overrides: [
-    {
-      //! Package is soon to be deprecated:
-      //! https://github.com/stylelint/postcss-css-in-js/issues/225
-      customSyntax: "@stylelint/postcss-css-in-js",
-      files: ["**/*.js", "**/*.ts", "**/*.jsx", "**/*.tsx"],
-      rules: {
-        "no-empty-first-line": null,
-        "value-keyword-case": null,
-      },
-    },
   ],
 };
 
-export = config;
+export default config;
