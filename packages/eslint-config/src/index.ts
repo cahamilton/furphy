ed4c3b94f46b8b81eadb3793659e6f22edc10015
Carl Hamilton <4007311+cahamilton@users.noreply.github.com>
2026-03-01 11:12:45 +0000

    feat!: update eslint config

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,46 +1,116 @@
 /** @format */
 
-import type { Linter } from "eslint";
+import eslint from "@eslint/js";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import { defineConfig, globalIgnores } from "eslint/config";
+import pluginImport from "eslint-plugin-import";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import pluginTypescript from "typescript-eslint";
 
-const rulesJS: Linter.RulesRecord = {
-  "import/prefer-default-export": "off",
-  "lines-between-class-members": "error",
-  "no-alert": "error",
-  "no-constant-condition": "error",
-  "no-debugger": "error",
-  "react/no-danger": "error",
-};
+const EXTENSIONS_JSX = "**/*.{jsx,tsx}";
+const EXTENSIONS_JAVASCRIPT = "**/*.{js,jsx,mjs}";
+const EXTENSIONS_TYPESCRIPT = "**/*.{ts,tsx,mts}";
 
-const rulesTS: Linter.RulesRecord = {
-  ...rulesJS,
-  "@typescript-eslint/lines-between-class-members": "error",
-  "react/prop-types": "off",
-};
-
-const config: Linter.Config = {
-  parser: "@babel/eslint-parser",
-  extends: ["airbnb", "airbnb/hooks", "prettier"],
-  rules: rulesJS,
-  overrides: [
-    {
-      files: "*.{ts,tsx}",
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
-      parserOptions: {
-        project: "tsconfig.json",
-        sourceType: "module",
+export default defineConfig([
+  globalIgnores(["**/*.d.ts", "**/package-lock.json"], "global-ignores"),
+  {
+    files: [EXTENSIONS_JAVASCRIPT, EXTENSIONS_TYPESCRIPT],
+    plugins: { eslint },
+    extends: [
+      eslint.configs["recommended"],
+      pluginImport.flatConfigs["recommended"],
+      pluginImport.flatConfigs["typescript"],
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
-      extends: [
-        "airbnb",
-        "airbnb/hooks",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "airbnb-typescript",
-        "prettier",
-      ],
-      rules: rulesTS,
     },
-  ],
-};
-
-export = config;
+    rules: {
+      "curly": ["error", "all"],
+      "eqeqeq": ["error", "always"],
+      "import/order": [
+        "error",
+        {
+          "groups": [["builtin", "external", "internal"]],
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+          "warnOnUnassignedImports": false,
+        },
+      ],
+      "import/no-cycle": ["error"],
+      "import/no-extraneous-dependencies": ["error"],
+      "import/no-unresolved": ["off"], // Too many false positives
+      "no-alert": ["error"],
+      "no-await-in-loop": ["error"],
+      "no-else-return": ["error"],
+      "no-lonely-if": ["error"],
+      "no-nested-ternary": ["error"],
+      "no-unneeded-ternary": ["error"],
+    },
+  },
+  {
+    files: [EXTENSIONS_TYPESCRIPT],
+    extends: [pluginTypescript.configs["strictTypeChecked"]],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      "@typescript-eslint/no-confusing-void-expression": ["off"],
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowAny: false,
+          allowBoolean: false,
+          allowNever: false,
+          allowNullish: false,
+          allowNumber: true,
+          allowRegExp: false,
+        },
+      ],
+    },
+  },
+  {
+    files: [EXTENSIONS_JSX],
+    extends: [
+      pluginReact.configs.flat["recommended"],
+      pluginReact.configs.flat["jsx-runtime"],
+      pluginReactHooks.configs.flat["recommended"],
+      pluginJsxA11y.flatConfigs["strict"],
+    ],
+    rules: {
+      "react/no-array-index-key": ["error"],
+      "react/no-danger": ["error"],
+    },
+    settings: {
+      react: {
+        version: "detect",
+        defaultVersion: "18",
+      },
+    },
+  },
+  {
+    files: ["**/*.json"],
+    // @ts-ignore: https://github.com/eslint/json/issues/213
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+]);
